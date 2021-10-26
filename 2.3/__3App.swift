7c5d38d6800e1d3c744e610d8375b18ce66b1b0e
KaterinaKugeneva <kugeneva1@gmail.com>
2021-10-26 09:59:45 +0300

    Initial Commit

---
+++ 2.3/__3App.swift
@@ -0,0 +1,17 @@
+//
+//  __3App.swift
+//  2.3
+//
+//  Created by Ekaterina Kugeneva on 26.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct __3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
